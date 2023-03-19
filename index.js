1b804cfd947c18279b81f33c10e1f1b7c3498b19
rashmin66 <hansi20ra@gmail.com>
2023-03-19 19:18:44 +0530

    Add files

---
+++ index.js
@@ -0,0 +1,8 @@
+// typed js
+const typed = new Typed('.multiple-text',{
+    strings:['Developer','Freelancer','Designer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
